c75489ec961d5328d35d223871ebbfbb23143fba
btzanella22 <bruno.zanella@quallecontrol.com.br>
2025-12-08 11:32:47 -0300

    Update appService.ts

--- appService.ts
+++ appService.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { ValkeyService } from '@toxicoder/nestjs-valkey';
+import { json } from 'stream/consumers';
+import { Cron } from '@nestjs/schedule';
 
 @Injectable()
 export class AppService {
@@ -7,11 +9,11 @@ export class AppService {
 
   // Cabeçalho
   private readonly DEFAULT_HEADERS = {
-    'Content-Type': 'conteudo',
-    'X-Api-Key': 'senha',
+    'Content-Type': 'application/json;charset=UTF-8',
+    'X-Api-Key': 'ZWYzYmMxZDYtOGZiMy00NTc2LWI3MDgtM2I5ZDMxODBlYzQ2OjczMDQ3',
   };
 
-  // Realiza busca os endpoints
+  // Busca dos endpoints
   private async searchAll(
     endpoint: String,
     dataFilter?: ((item: any) => boolean) | null,
@@ -42,29 +44,60 @@ export class AppService {
     return records;
   }
 
-  // Retorna o cache armazenado
+  // Getter do cache
   private async getCachedValue(key: string): Promise<string | null> {
     return this.valkeyService.get(key);
   }
 
-  // Insere o cache
-  private async setCachedValue(key: string, value: string, ttl = 10) {
+  // Setter do cache
+  private async setCachedValue(key: string, value: string, ttl = 300) {
     return this.valkeyService.setEx(key, value, ttl);
   }
 
+  // Retorna customers
   public async getCustomers() {
     const cacheKey = 'customers_cache'; // Cria chave do cache
     const cached = await this.getCachedValue(cacheKey); // Cria armazenamento p/ cache
-    if (cached) return JSON.parse(cached); // Se possui cache, converte em JSON, se não, busca os dados diretamente da API e armazena no cache criado
+
+    // Se possui cache, converte em JSON
+    if (cached) {
+      this.updateCacheCustomers(cacheKey); // Atualiza o cache
+      return JSON.parse(cached);
+    }
+
+    // Se não, busca os dados diretamente da API e armazena no cache criado
     const response = await this.searchAll('customers');
     await this.setCachedValue(cacheKey, JSON.stringify(response));
+
+    // for(let item of response){
+    //   console.log(item.id);
+    // }
+
     return response;
   }
 
+  // Atualiza o cache
+  private async updateCacheCustomers(cacheKey: string) {
+    try {
+      const response = await this.searchAll('customers');
+      await this.setCachedValue(cacheKey, JSON.stringify(response));
+    } catch (err) {
+      console.error('Falha!', err);
+    }
+  }
+
+  // Retorna orders (Telemetria | Abertura de Chamado)
   public async getOrders() {
     const cacheKey = 'orders_cache'; // Cria chave do cache
     const cached = await this.getCachedValue(cacheKey); // Cria armazenamento p/ cache
-    if (cached) return JSON.parse(cached); // Se possui cache, converte em JSON, se não, busca os dados diretamente da API e armazena no cache criado
+
+    // Se possui cache, converte em JSON
+    if (cached) {
+      this.updateCacheOrders(cacheKey);
+      return JSON.parse(cached);
+    }
+
+    // Se não, busca os dados diretamente da API e armazena no cache criado
     const response = await this.searchAll(
       'orders',
       (order) => order.service?.id === 'MzE5NjIxOjczMDQ3',
@@ -73,19 +106,58 @@ export class AppService {
     return response;
   }
 
+  // Atualiza o cache
+  private async updateCacheOrders(cacheKey: string) {
+    try {
+      const response = await this.searchAll(
+        'orders',
+        (order) => order.service?.id === 'MzE5NjIxOjczMDQ3',
+      );
+      await this.setCachedValue(cacheKey, JSON.stringify(response));
+    } catch (err) {
+      console.error('Falha!', err);
+    }
+  }
+
+  // Retorna services
   public async getServices() {
     const cacheKey = 'services_cache'; // Cria chave do cache
     const cached = await this.getCachedValue(cacheKey); // Cria armazenamento p/ cache
-    if (cached) return JSON.parse(cached); // Se possui cache, converte em JSON, se não, busca os dados diretamente da API e armazena no cache criado
+
+    // Se possui cache, converte em JSON
+    if (cached) {
+      this.updateCacheServices(cacheKey);
+      return JSON.parse(cached);
+    }
+
+    // Se não, busca os dados diretamente da API e armazena no cache criado
     const response = await this.searchAll('services');
     await this.setCachedValue(cacheKey, JSON.stringify(response));
     return response;
   }
 
+  // Atualiza o cache
+  private async updateCacheServices(cacheKey: string) {
+    try {
+      const response = await this.searchAll('services');
+      await this.setCachedValue(cacheKey, JSON.stringify(response));
+    } catch (err) {
+      console.error('Falha!', err);
+    }
+  }
+
+  // Retorna tasks (scheduled)
   public async getTasks() {
     const cacheKey = 'tasks_cache'; // Cria chave do cache
     const cached = await this.getCachedValue(cacheKey); // Cria armazenamento p/ cache
-    if (cached) return JSON.parse(cached); // Se possui cache, converte em JSON, se não, busca os dados diretamente da API e armazena no cache criado
+
+    // Se possui cache, converte em JSON
+    if (cached) {
+      this.updateCacheTasks(cacheKey);
+      return JSON.parse(cached);
+    }
+
+    // Se não, busca os dados diretamente da API e armazena no cache criado
     const response = await this.searchAll(
       'tasks',
       (task) => task.status === 'scheduled',
@@ -94,13 +166,51 @@ export class AppService {
     return response;
   }
 
+  // Atualiza o cache
+  private async updateCacheTasks(cacheKey: string) {
+    try {
+      const response = await this.searchAll(
+        'tasks',
+        (task) => task.status === 'scheduled',
+      );
+      await this.setCachedValue(cacheKey, JSON.stringify(response));
+    } catch (err) {
+      console.error('Falha!', err);
+    }
+  }
+
+  // Retorna locations
   public async getLocationsByCustomer(customerId: string) {
     const cacheKey = `locations_cache:${customerId}`; // Cria chave do cache
     const cached = await this.getCachedValue(cacheKey); // Cria armazenamento p/ cache
-    if (cached) return JSON.parse(cached); // Se possui cache, converte em JSON, se não, busca os dados diretamente da API e armazena no cache criado
+
+    // Se possui cache, converte em JSON
+    if (cached) {
+      this.updateCacheLocationsByCustomer(cacheKey, customerId);
+      return JSON.parse(cached);
+    }
+
+    // Se não, busca os dados diretamente da API e armazena no cache criado
     const response = await this.searchAll(`customers/${customerId}/locations`);
     await this.setCachedValue(cacheKey, JSON.stringify(response));
     return response;
+  }
+
+  // Atualiza o cache
+  private async updateCacheLocationsByCustomer(
+    cacheKey: string,
+    customerId: string,
+  ) {
+    try {
+      const response = await this.searchAll(
+        `customers/${customerId}/locations`,
+      );
+      await this.setCachedValue(cacheKey, JSON.stringify(response));
+      return response;
+    } catch (err) {
+      console.error('Falha!', err);
+      return [];
+    }
   }
 
   public async getLocationByCustomer(customerId: string, locationId: string) {
@@ -111,5 +221,36 @@ export class AppService {
     const location = locations.filter((l) => l.id === locationId)[0];
     await this.setCachedValue(cacheKey, JSON.stringify(location));
     return location;
+  }
+
+  private async updateCacheAllLocationsByCustomer() {
+    try {
+      const customers: Array<any> = await this.searchAll('customers');
+
+      for (const c of customers) {
+        const customerId = c.id;
+
+        const locations: Array<any> = await this.updateCacheLocationsByCustomer(
+          `locations_cache:${customerId}`,
+          customerId,
+        );
+
+        for(const l of locations){
+          const key = `locations_cache:${customerId}_location:${l.id}`;
+          await this.setCachedValue(key, JSON.stringify(l));
+        }
+      }
+    } catch (err) {
+      console.error('Falha ' + err);
+    }
+  }
+
+  @Cron('0 */5 * * * *')
+  public async schedule() {
+    await this.updateCacheCustomers('customers_cache');
+    await this.updateCacheOrders('orders_cache');
+    await this.updateCacheServices('services_cache');
+    await this.updateCacheTasks('tasks_cache');
+    await this.updateCacheAllLocationsByCustomer();
   }
 }
